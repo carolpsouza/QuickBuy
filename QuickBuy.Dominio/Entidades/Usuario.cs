4cb15ffab273b59fb1aef4f1a54549e224804e9f
Carolina Pinheiro de Souza <carolpinheirosouza@gmail.com>
2019-07-29 22:29:23 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        //um usuário pode ter 0 ou muitos pedidos
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
